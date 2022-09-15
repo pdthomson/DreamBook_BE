367c26c224eb42bec412e8d702e457cf9cd5b750
Nikky Rojas <rojas7313@gmail.com>
2022-09-15 09:31:12 -0600

    add to test

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Blog, type: :model do
     # it {should validate_presence_of :title }
     # it {should validate_presence_of :body }
     # it {should validate_presence_of :user_id }
+    # it {should validate_presence_of :status}
     it { should have_many :comments }
     it { should belong_to :user}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do 
-    # it {should validate_presence_of :name }
+    # it {should validate_presence_of :user_name }
     # it {should validate_presence_of :email }
     # it {should validate_uniqueness_of :email }
     it { should have_many :blogs }
