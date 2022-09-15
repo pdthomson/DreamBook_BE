3449937ae2cc4aea6d187c41975539725a523886
Shirley DeCesari <shirley.decesari@gmail.com>
2022-09-15 15:20:36 -0700

    Test: initial_blog_serializer

---
+++ spec/factories/blogs.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do 
+  factory :blog do 
+    title { Faker::Games::Zelda.item }
+    body { Faker::Movies::VForVendetta.quote }
+    status { Faker::Number.within(range: 0..1) }
+    user_id { Faker::Number.within(range: 0..100)}
+    # after(:create) do |blog|
+    #   create_list :comment, 2, blog_id: blog
+    # end 
+  end 
+end 
+
