fe36772f29e32519c3c4ea5588ad828e821ed560
Bryce Simonds <bsimonds14@gmail.com>
2022-09-19 15:54:13 -0600

    Refactor: created get_url helper method

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,16 +1,23 @@
 class MovieService 
   class << self 
     def movie_keyword_recommendations(keyword)
-      response = conn.get("/3/search/movie?api_key=#{ENV["movie_api_key"]}&language=en-US&query=#{keyword}")
-      json = JSON.parse(response.body, symbolize_names: true)
+      get_url("/3/search/movie?api_key=#{ENV["movie_api_key"]}&language=en-US&query=#{keyword}")
+    end
+
+    def get_url(url, keyword = nil)
+      conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+        faraday.params['X-API-Key'] = ENV["movie_api_key"]
+      end
+      response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
     end
   end 
-
-  private 
-
-  def self.conn
-    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
-      faraday.params['X-API-Key'] = ENV["movie_api_key"]
-    end
-  end
 end
+
+
+
+
+
+
+
+
