e9f1666cf51ffada22a24c3651ae7f34b9dafedf
Bryce Simonds <bsimonds14@gmail.com>
2022-09-19 17:44:33 -0600

    Serializer sends JSONM

--- app/controllers/api/v1/movie_recommendations_controller.rb
+++ app/controllers/api/v1/movie_recommendations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MovieRecommendationsController < ApplicationController
   def index
     api_call = MovieService.movie_keyword_recommendations(params[:keyword])
-    render json: MovieSerializer.get_movie_recommendations
+    render json: MovieSerializer.get_movie_recommendations(api_call)
   end
 end
 
