fafd70212ecf8392bc13e4f7ba6a6b4bc39c2bc0
Bryce Simonds <bsimonds14@gmail.com>
2022-09-19 15:45:21 -0600

    Added api endpoint for movie search

---
+++ app/services/movie_service.rb
@@ -0,0 +1,17 @@
+class MovieService 
+  class << self 
+    def movie_keyword_recommendations(keyword)
+      response = conn.get("/3/search/movie?api_key=#{ENV["movie_api_key"]}&language=en-US&query=#{keyword}")
+      json = JSON.parse(response.body, symbolize_names: true)
+      binding.pry
+    end
+  end 
+
+  private 
+
+  def self.conn
+    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.params['X-API-Key'] = ENV["movie_api_key"]
+    end
+  end
+end
