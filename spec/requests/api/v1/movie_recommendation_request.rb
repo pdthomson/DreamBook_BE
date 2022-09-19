04bb2907b323fb1c7d421686f8b4bc50c5e241cf
Bryce Simonds <bsimonds14@gmail.com>
2022-09-19 15:39:23 -0600

    Test: returns list of recommendated movies based off keyword

--- spec/requests/api/v1/movie_recommendation_request.rb
+++ spec/requests/api/v1/movie_recommendation_request.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'All Movie Recommendations API' do
   it 'returns list of movie recommendations based off of keyword', :vcr do 
-    parsed_json = MovieService.movie_keyword_recommendations("pink bird")
+    parsed_json = MovieService.movie_keyword_recommendations("pink")
     expect(parsed_json).to be_a Hash
     expect(parsed_json[:results]).to be_a Array
 
     movie_recommendation_data = parsed_json[:results][0]
-    expect(movie_recommendation_data).to include(:original_title, :id, :vote_average)
-    expect(movie_recommendation_data[:original_title]).to be_a String
+    expect(movie_recommendation_data).to include(:title, :description)
+    expect(movie_recommendation_data[:title]).to be_a String
+    expect(movie_recommendation_data[:description]).to be_a String
     expect(movie_recommendation_data[:id]).to be_a Integer
-    expect(movie_recommendation_data[:vote_average]).to be_a Float
   end
 end
