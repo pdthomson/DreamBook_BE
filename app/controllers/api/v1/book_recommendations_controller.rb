5c9a6c1ac2bde340e9798682892b6a8972e44870
Tyler Caudill <teecaud@hotmail.com>
2022-09-20 16:33:42 -0500

    index action for controller complete

--- app/controllers/api/v1/book_recommendations_controller.rb
+++ app/controllers/api/v1/book_recommendations_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::BookRecommendationsController < ApplicationController
 
   def index
     api_call = BookService.book_keyword_recommendations(params[:keyword])
-
     render json: BookSerializer.get_book_recommendation(api_call)
   end
 end
