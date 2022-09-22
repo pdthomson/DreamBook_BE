a5ad6eaeb3115d0ab34f88505099572600cb8ee1
Nikky Rojas <rojas7313@gmail.com>
2022-09-22 11:59:15 -0600

    deleted all them seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+blog1 = Blog.create(title: 'dream about code', body: 'had a dream about code that can fix a bug and it worked!', user_id: 1, status: 1, keyword: 'work')
+
+Comment.create(comment_text: 'sounds like a nighmare!', user_id: 2, blog_id: blog1.id )
+Comment.create(comment_text: 'thats happened to me too!', user_id: 3, blog_id: blog1.id )
